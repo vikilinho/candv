a68f7b6b99ccfaf03d0763a15499dfeefcf36484
victor <dvicxy@gmail.com>
2022-08-07 13:39:19 +0100

    mobile display

--- lib/views/mobile_page.dart
+++ lib/views/mobile_page.dart
@@ -1,6 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+final String logo = 'images/logo.svg';
 
 class MobilePage extends StatefulWidget {
   const MobilePage({Key? key}) : super(key: key);
@@ -10,6 +16,11 @@ class MobilePage extends StatefulWidget {
 }
 
 class _MobilePageState extends State<MobilePage> {
+  final Widget candv = SvgPicture.asset(
+    'images/Ellipse.svg',
+    semanticsLabel: 'cv',
+    height: 50,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,14 +41,20 @@ class _MobilePageState extends State<MobilePage> {
             )),
             ListTile(
               title: const Text('Gallery'),
-              leading: const Icon(Icons.photo_library),
+              leading: const Icon(
+                Icons.photo_library,
+                color: Colors.red,
+              ),
               onTap: () {
                 Navigator.pop(context);
               },
             ),
             ListTile(
               title: const Text('Direction'),
-              leading: const Icon(Icons.directions),
+              leading: const Icon(
+                Icons.directions,
+                color: Colors.teal,
+              ),
               onTap: () {
                 Navigator.pop(context);
               },
@@ -46,10 +63,124 @@ class _MobilePageState extends State<MobilePage> {
         ),
       ),
       appBar: AppBar(
-        title: const Text('C&V'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.red,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
       ),
-      body: const Center(
-        child: Text('Mobile'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  FadeAnimatedText(
+                    textStyle: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.04,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink,
+                    ),
+                    'Chigozie',
+                  ),
+                  FadeAnimatedText(
+                      textStyle: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * 0.04,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                      ),
+                      '&'),
+                  FadeAnimatedText(
+                      textStyle: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * 0.04,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                      'Chigozie & Victor'),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width * 0.99,
+              margin: EdgeInsets.all(50.0),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/c&v.png"),
+                      fit: BoxFit.cover), //change image fill type
+                  shape: BoxShape.circle),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                      speed: Duration(milliseconds: 30),
+                      textStyle: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * 0.03,
+                        fontStyle: FontStyle.italic,
+                      ),
+                      'You are highly invited to celebrate our wedding.'),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: AnimatedTextKit(
+                animatedTexts: [
+                  TypewriterAnimatedText(
+                    speed: Duration(milliseconds: 60),
+                    textStyle: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * 0.03,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                    "Saturday, September 3, 2022, 2PM",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Wrap(children: [
+                Text(
+                  "Address:",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.height * 0.02,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text("Agbada Nenwe, Aniri LGA. Enuga",
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.02,
+                      fontStyle: FontStyle.italic,
+                    )),
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
