76645aa009910d02435c05f247586b30e4835731
victor <dvicxy@gmail.com>
2022-08-07 12:02:08 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:candv/views/desktop_page.dart';
+import 'package:candv/views/mobile_page.dart';
 import 'package:candv/views/responsive_page.dart';
+import 'package:candv/views/tablet_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,11 +28,10 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home:  ResponsivePage(
-        mobilePage: const MobilePage(),
-        tabletPage: const TabletPage(),
-        desktopPage: const DesktopPage(),
-      ),
+      home: const ResponsivePage(
+        mobilePage: MobilePage(),
+        tabletPage: TabletPage(),
+        desktopPage: DesktopPage(),
       ),
     );
   }
