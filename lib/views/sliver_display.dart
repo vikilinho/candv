215e01712676733a5df81c75739955a87e6b50a5
victor <dvicxy@gmail.com>
2022-08-26 15:11:57 +0100

    new commit

--- lib/views/sliver_display.dart
+++ lib/views/sliver_display.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class SliverMobile extends StatefulWidget {
@@ -18,14 +19,52 @@ class _SliverMobileState extends State<SliverMobile> {
         slivers: [
           // ignore: prefer_const_constructors
           SliverAppBar(
-              expandedHeight: 200,
+              expandedHeight: 100,
               backgroundColor: const Color(0xff700548),
               flexibleSpace: const FlexibleSpaceBar(
                   // background: Image.asset("images/c&v.png"),
                   )),
           SliverToBoxAdapter(
             child: Column(
-              children: [],
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          FadeAnimatedText(
+                            textStyle: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).size.height * 0.04,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink,
+                            ),
+                            'Chigozie',
+                          ),
+                          FadeAnimatedText(
+                              textStyle: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.04,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                              ),
+                              '&'),
+                          FadeAnimatedText(
+                              textStyle: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.04,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                              'Chigozie & Victor'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           )
         ],
